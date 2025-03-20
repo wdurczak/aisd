e6c4e7a2b5f6e053d2633558e5c23ca3d6f91783
Wojciech Durczak <wojciech.durczak@gmail.com>
2025-03-20 10:14:54 +0100

    Add shell sort

---
+++ shell_sort.py
@@ -0,0 +1,31 @@
+def sedgewick_gaps(n):
+    gaps = []
+    k=0
+    while True:
+        if k % 2==0:
+            gap = 9 *(2**k - 2 ** (k // 2)) + 1
+        else:
+            gap = 8 *2 ** k - 6 * 2 ** ((k + 1) // 2)+1
+        if gap>=n:
+            break
+        gaps.append(gap)
+        k += 1
+
+    return gaps[::-1]
+
+def shell_sort(data):
+    n = len(data)
+    gaps = sedgewick_gaps(n)
+    for gap in gaps:
+        for i in range(gap, n):
+            temp = data[i]
+            j=i
+            while j >= gap and data[j - gap] > temp:
+                data[j] = data[j - gap]
+                j -= gap
+            data[j] = temp
+
+data = [45, 12, 89, 33, 76, 17, 92, 5, 31, 63]
+print("bazowa tablica: ", data)
+shell_sort(data)
+print("posortowana tablica: ", data)
